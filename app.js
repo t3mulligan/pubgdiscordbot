0c24f93c7cc3867db9782bf695081fdaa4525654
Tim Mulligan <t3mulligan@gmail.com>
2018-08-17 17:36:22 -0400

    oops

--- app.js
+++ app.js
@@ -2,3 +2,9 @@
 const pubg = require('pubg.js');
 const config = require('./config.json');
 const client = new pubg.Client(config.pubg.apiKey, config.pubg.region);
+
+
+// Get a single player using their name
+const player = client.getPlayer({name: 'solid__snake'})
+    .then(player => console.log(player))
+    .catch(error => console.log(error));
